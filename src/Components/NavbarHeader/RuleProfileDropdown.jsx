33c8f0c71d2708e23cab3539f4b5b7d73046215a
Govind1732 <146473888+Govind1732@users.noreply.github.com>
2024-04-11 22:14:31 +0530

    Update RuleProfileDropdown.jsx

--- src/Components/NavbarHeader/RuleProfileDropdown.jsx
+++ src/Components/NavbarHeader/RuleProfileDropdown.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { NavDropdown, NavLink, Dropdown } from "react-bootstrap";
+import { NavDropdown, NavLink, DropdownButton, ButtonGroup, Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import './RuleProfileDropdown.css'
+// import './RuleProfileDropdown.css'
 
 const RuleProfileDropdown = ({ selectedOption }) => {
   const [isOpen, setIsOpen] = useState(false);
-
-  const handleMouseEnter = () => setIsOpen(true);
-  const handleMouseLeave = () => setIsOpen(false);
+  const [isOpenRant, setIsOpenRant] = useState(false);
+  const [isOpenCorp, setIsOpenCorp] = useState(false);
+  const [isOpenAllProject, setIsOpenAllProject] = useState(false);
 
   return (
     <>
@@ -15,39 +15,44 @@ const RuleProfileDropdown = ({ selectedOption }) => {
       <NavDropdown
         title="Rule Profile"
         show={isOpen}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        className={`dropdown-button ${ selectedOption === "RuleProfile" ? "active" : ""}`}>
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+        className={`dropdown-button ${selectedOption === "RuleProfile" ? "active" : ""} `}>
 
-        {/* <NavLink className="dropdown-item"> */}
-          <NavDropdown
-            title="RantDT"
-            drop="end"
-            >
-            <NavLink as={Link} to="/rantdt" className="dropdown-item">Submit Request</NavLink>
-            <NavLink as={Link} to="/rantdtviewedit" className="dropdown-item">View/Edit Request</NavLink>
-          </NavDropdown>
-        {/* </NavLink> */}
+        <NavDropdown
+          title="RantDT"
+          drop="end"
+          show={isOpenRant}
+          onMouseEnter={() => setIsOpenRant(true)}
+          onMouseLeave={() => setIsOpenRant(false)}
+        // className={`dropdown-menu ${selectedOption === "RuleProfile" ? "active" : ""} `}
+        >
+          <NavLink as={Link} to="/dataQuality/rantdt" className={`dropdown-item ${selectedOption === "RuleProfile" ? "active" : ""} `}>Submit Request</NavLink>
+          <NavLink as={Link} to="/dataQuality/rantdtviewedit" className="dropdown-item">View/Edit Request</NavLink>
+        </NavDropdown>
 
-        {/* <NavLink className="dropdown-item"> */}
-          <NavDropdown
-            title="Corp Data"
-            drop="end"
-            >
-            <NavLink as={Link} to="/corpData" className="dropdown-item">Submit Request</NavLink>
-            <NavLink as={Link} to="/corpDataviewedit" className="dropdown-item">View/Edit Request</NavLink>
-          </NavDropdown>
-        {/* </NavLink> */}
+        <NavDropdown
+          title="Corp Data"
+          drop="end"
+          show={isOpenCorp}
+          onMouseEnter={() => setIsOpenCorp(true)}
+          onMouseLeave={() => setIsOpenCorp(false)}
+        // className={`dropdown-menu ${selectedOption === "RuleProfile" ? "active" : ""} `}
+        >
+          <NavLink as={Link} to="/dataQuality/corpData" className="dropdown-item">Submit Request</NavLink>
+          <NavLink as={Link} to="/dataQuality/corpDataviewedit" className="dropdown-item">View/Edit Request</NavLink>
+        </NavDropdown>
 
-        {/* <NavLink className="dropdown-item"> */}
-          <NavDropdown
-            title="All Projects"
-            drop="end"
-            >
-            <NavLink as={Link} to="/allProjects" className="dropdown-item">Submit Request</NavLink>
-            <NavLink as={Link} to="/allProjectsviewedit" className="dropdown-item">View/Edit Request</NavLink>
-          </NavDropdown>
-        {/* </NavLink> */}
+        <NavDropdown
+          title="All Projects"
+          drop="end"
+          show={isOpenAllProject}
+          onMouseEnter={() => setIsOpenAllProject(true)}
+          onMouseLeave={() => setIsOpenAllProject(false)}
+        >
+          <NavLink as={Link} to="/dataQuality/allProjects" className="dropdown-item">Submit Request</NavLink>
+          <NavLink as={Link} to="/dataQuality/allProjectsviewedit" className="dropdown-item">View/Edit Request</NavLink>
+        </NavDropdown>
 
       </NavDropdown>
     </>

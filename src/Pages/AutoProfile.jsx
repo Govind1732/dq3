36a3456905919cc2576d70909f8fbda39433caaa
Govind1732 <146473888+Govind1732@users.noreply.github.com>
2024-04-26 15:58:41 +0530

    Update AutoProfile.jsx

--- src/Pages/AutoProfile.jsx
+++ src/Pages/AutoProfile.jsx
@@ -311,6 +311,140 @@ const AutoProfile = ({ onData }) => {
 
 export default AutoProfile;
 
+import React, { useState, useRef } from "react";
+import { Container, Row, Col, Form, Button, Breadcrumb, Modal, Alert } from "react-bootstrap";
+import axios from "axios";
+import { GridLoader } from "react-spinners";
+// import './AutoProfile.css'
+import Backdrop from '@mui/material/Backdrop';
+// import CircularProgress from '@mui/material/CircularProgress';
+
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
+
+const AutoProfile = ({ onData }) => {
+  const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
+  const [modalTitle, setModalTitle] = useState('')
+  const [modalBody, setModalBody] = useState('')
+
+  const fileInputRef = useRef(null);
+  const handleClose = () => {
+    setShow(false);
+    resetHandler()
+  }
+
+  const handleUpload = () => {
+    const file1 = fileInputRef.current.files[0];
+    console.log(file1)
+    if (!file1) {
+      alert('Please select a file');
+      return;
+    }
+    setLoading(true);
+
+    axios.post('https://tdcldizcva002.ebiz.verizon.com:8001/mle/MLESelfServe/', file1, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+      .then(response => {
+        console.log(response.data);
+        setLoading(false);
+        setModalTitle(response.data.status)
+        setModalBody(response.data.message)
+        setShow(true)
+      })
+      .catch(error => {
+        console.error('Error uploading file: ', error);
+        setLoading(false);
+        setModalTitle(error.status)
+        setModalBody(error.message)
+        setShow(true)
+
+      });
+  };
+
+  const resetHandler = (e) => {
+    fileInputRef.current.value = '';
+  };
+
+  return (
+    <>
+
+      <Container fluid className=''>
+        <Container fluid className="my-2 pt-3">
+          <Breadcrumb>
+            <Breadcrumb.Item active>Profiling Services</Breadcrumb.Item>
+            <Breadcrumb.Item active>Auto Profile</Breadcrumb.Item>
+            <Breadcrumb.Item active><span className='fw-bold'>Submit Request</span></Breadcrumb.Item>
+
+          </Breadcrumb>
+        </Container>
+
+        <Container fluid className="mx-8 px-8 mb-2">
+          <Row className="justify-content-center align-items-center my-5">
+            <Col xl={5}>
+              <div className="px-5 py-3">
+                <h2 className="mb-4 text-center" style={{ color: '#EE0000' }}>Auto Profile</h2>
+
+
+                <h6>dqaas_auto_prfl_mtd_&lt;proj_name&gt;.csv</h6>
+
+                <Form.Group controlId="file1" className="my-3">
+
+                  <Form.Control type="file" accept=".csv" ref={fileInputRef} />
+                </Form.Group>
+
+                <a href="/dataQuality/assets/dqaas_auto_prfl_mtd_proj_name.csv" download className="text-decoration-none text-danger">
+                  Download sample Template(CSV)
+                </a>
+                <div className="d-flex justify-content-center my-3">
+                  <Button variant="outline-dark" onClick={resetHandler} className='mx-2' style={{ borderRadius: '25px' }}>
+                    Reset
+                  </Button>
+                  <Button variant="dark" type="submit" onClick={handleUpload} className="mx-2" style={{ borderRadius: '25px' }}>
+                    Submit
+                  </Button>
+                </div>
+
+
+              </div>
+
+            </Col>
+          </Row>
+        </Container>
+        <Modal show={show} onHide={() => handleClose()} dialogClassName="modal-90w" variant="success" as={Alert}>
+          <Modal.Header closeButton>
+            <Modal.Title>{modalTitle}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{modalBody}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+
+          </Modal.Footer>
+        </Modal>
+      </Container>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        {/* <CircularProgress color="inherit" /> */}
+        <Container className="loading-overlay">
+          <GridLoader color="#ff0000" loading={loading} cssOverride={override} size={20} aria-label="Loading Spinner" data-testid='loader' />
+        </Container>
+      </Backdrop>
+    </>
+  );
+};
+
+export default AutoProfile;
+
 
 
 

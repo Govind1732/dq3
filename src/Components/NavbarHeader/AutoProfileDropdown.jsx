1d011c7346275ade513b7a89ca2aa9833bbe5a66
Govind1732 <146473888+Govind1732@users.noreply.github.com>
2024-05-03 10:47:29 +0530

    Update AutoProfileDropdown.jsx

--- src/Components/NavbarHeader/AutoProfileDropdown.jsx
+++ src/Components/NavbarHeader/AutoProfileDropdown.jsx
@@ -2,26 +2,36 @@ import React, { useState } from "react";
 import { NavDropdown, NavLink } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const AutoProfileDropdown = ({ selectedOption}) => {
+const AutoProfileDropdown = ({ selectedOption,endpoint,selectedProfileOption}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleMouseEnter = () => setIsOpen(true);
   const handleMouseLeave = () => setIsOpen(false);
 
+  const style = {
+    margin: 0,
+    padding: 0,
+    boxsizing: "border-box",
+  };
   return (
     <>
     <NavDropdown
-      title="Auto Profile"
-      show={isOpen} 
+      title={<span style={{fontWeight:"normal"}}>Auto Profile</span>}
+      show={isOpen}
+      drop="end" 
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className={`dropdown-button ${selectedOption === "AutoProfile" ? "active" : ""}`}
-    >
-      <NavLink as={Link} to="/autoProfile" className="dropdown-item">
+      style={style}
+      className={`dropdown-item ${selectedProfileOption === "AutoProfile" ? "active" : ""}`}
+      >
+      <NavLink as={Link} to="/dataQuality/autoProfile" className={`dropdown-item ${endpoint === "/dataQuality/autoProfile" ? "active" : ""}`}>
         Submit Request
       </NavLink>
-      <NavLink as={Link} to="/autoviewedit" className="dropdown-item">
-        View/Edit Request
+      <NavLink as={Link} to="/dataQuality/autoviewedit" className={`dropdown-item ${endpoint === "/dataQuality/autoviewedit" ? "active" : ""}`}>
+        View/Edit Matadata
+      </NavLink>
+      <NavLink as={Link} to="/dataQuality/autoProfileAPI" className={`dropdown-item ${endpoint === "/dataQuality/autoProfileAPI" ? "active" : ""}`}>
+        AutoProfileAPI
       </NavLink>
     </NavDropdown>
     </>

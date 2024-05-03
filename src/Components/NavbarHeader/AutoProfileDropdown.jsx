190fbc68fd036577c83f49b91d3fc591a771b512
Govind1732 <146473888+Govind1732@users.noreply.github.com>
2024-05-03 11:01:46 +0530

    Update AutoProfileDropdown.jsx

--- src/Components/NavbarHeader/AutoProfileDropdown.jsx
+++ src/Components/NavbarHeader/AutoProfileDropdown.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { NavDropdown, NavLink } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const AutoProfileDropdown = ({ selectedOption,endpoint,selectedProfileOption}) => {
+const AutoProfileDropdown = ({ selectedOption, endpoint, selectedProfileOption }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleMouseEnter = () => setIsOpen(true);
@@ -11,33 +11,30 @@ const AutoProfileDropdown = ({ selectedOption,endpoint,selectedProfileOption}) =
   const style = {
     margin: 0,
     padding: 0,
-    boxsizing: "border-box",
+    boxSizing: "border-box",
   };
+
   return (
-    <>
     <NavDropdown
-      title={<span style={{fontWeight:"normal"}}>Auto Profile</span>}
+      title={<span style={{ fontWeight: "normal" }}>Auto Profile</span>}
       show={isOpen}
-      drop="end" 
+      drop="end"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       style={style}
       className={`dropdown-item ${selectedProfileOption === "AutoProfile" ? "active" : ""}`}
-      >
+    >
       <NavLink as={Link} to="/dataQuality/autoProfile" className={`dropdown-item ${endpoint === "/dataQuality/autoProfile" ? "active" : ""}`}>
         Submit Request
       </NavLink>
       <NavLink as={Link} to="/dataQuality/autoviewedit" className={`dropdown-item ${endpoint === "/dataQuality/autoviewedit" ? "active" : ""}`}>
-        View/Edit Matadata
+        View/Edit Metadata
       </NavLink>
       <NavLink as={Link} to="/dataQuality/autoProfileAPI" className={`dropdown-item ${endpoint === "/dataQuality/autoProfileAPI" ? "active" : ""}`}>
         AutoProfileAPI
       </NavLink>
     </NavDropdown>
-    </>
   );
 };
 
 export default AutoProfileDropdown;
-Warning: validateDOMNesting(...): <a> cannot appear as a descendant of <a>.
-    at a
